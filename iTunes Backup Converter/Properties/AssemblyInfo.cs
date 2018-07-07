d42ef8fe0ea3dc40c97884834d0156f6905f9ce1
iSan4eZ <temnyip@gmail.com>
2018-07-07 15:51:53 +0300

    Credentials added

--- iTunes Backup Converter/Properties/AssemblyInfo.cs
+++ iTunes Backup Converter/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("iTunes Backup Converter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Utility that allows you to downgrade iTunes iOS backups")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("iSan4eZ")]
 [assembly: AssemblyProduct("iTunes Backup Converter")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("© iSan4eZ, 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
